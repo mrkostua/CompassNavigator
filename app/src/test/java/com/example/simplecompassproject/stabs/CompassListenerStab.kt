72dd9dfe103e9e1c388aaa5e1572fd35130053e3
kprysiazhnyi <kprysiazhnyi@corelogic.pl>
2019-07-18 01:57:31 +0200

    refactoring after running lint; updating graphics;

--- app/src/test/java/com/example/simplecompassproject/stabs/CompassListenerStab.kt
+++ app/src/test/java/com/example/simplecompassproject/stabs/CompassListenerStab.kt
@@ -1,10 +1,12 @@
 package com.example.simplecompassproject.stabs
 
 import com.example.simplecompassproject.util.ui.compass.CompassSensorsService
+import org.jetbrains.annotations.TestOnly
 
 /**
  * Created by Kostiantyn Prysiazhnyi on 7/17/2019.
  */
+@TestOnly
 class CompassListenerStab : CompassSensorsService.CompassListener {
     var updatedAzimuth: Float? = null
     var isSensorEventOccurred: Boolean = false
